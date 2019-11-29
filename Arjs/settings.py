55c3ec1879b86198ae2c91657375306b46e95933
PrakhBanana <Prakharsaxena11111@gmail.com>
2019-11-29 23:32:58 +0900

    Ar

--- Arjs/settings.py
+++ Arjs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e6!$uo$09tz05xqze_%wwzs+o#6#&g2)7_bpix%oka2=98)_$5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
